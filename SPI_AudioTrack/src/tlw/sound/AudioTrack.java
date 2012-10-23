93b9dba0f3781c433546a60521b72d3cdbf9a598
tlw_ray <liwei.tang@magustek.com>
2012-10-24 00:05:49 +0800

    由于Android和J2SE的音频播放接口不一致，通过SPI方式解决。建立统一音频接口tlw.sound.AudioTrack

---
+++ SPI_AudioTrack/src/tlw/sound/AudioTrack.java
@@ -0,0 +1,18 @@
+package tlw.sound;
+
+public interface AudioTrack {
+	static final int SAMPLE_RATE_11025=11025;
+	static final int SAMPLE_RATE_22050=22050;
+	static final int SAMPLE_RATE_44100=44100;
+	static final int CHANNEL_MONO=2;
+	static final int CHANNEL_STEREO=3;
+	static final int BIT_8=8;
+	static final int BIT_16=16;
+	void write(byte[] audioData, int off, int len);
+	void play();
+	boolean isActive();
+	boolean isOpen();
+	void close();
+	int available();
+	int getBufferSize();
+}
---
+++ SPI_AudioTrack/src/tlw/sound/AudioTrackFactory.java
@@ -0,0 +1,48 @@
+package tlw.sound;
+
+import java.util.ServiceLoader;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+
+public class AudioTrackFactory {
+	public static void main(String[] args){
+		AudioTrack at=AudioTrackFactory.getAudioTrack(
+				AudioTrack.SAMPLE_RATE_11025, 
+				AudioTrack.BIT_16, 
+				AudioTrack.CHANNEL_MONO, 
+				true, false);
+		System.out.println(at);
+	}
+	private static ServiceLoader<? extends AudioTrack> serviceLoader = ServiceLoader.load(AudioTrack.class); 
+	//interface SourceDataLine supporting format PCM_SIGNED 11025.0 Hz, 16 bit, mono, 2 bytes/frame, little-endian
+	public static AudioTrack getAudioTrack(float sampleRate,
+            int sampleSizeInBits,
+            int channels,
+            boolean signed,
+            boolean bigEndian){
+		for(AudioTrack service : serviceLoader){
+			return service;
+		}
+		return null;
+		
+//		AudioTrack audioTrack=null;
+		
+		//swing
+//		try {
+//			AudioFormat audioFormat=new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
+//			DataLine.Info info = new DataLine.Info(SourceDataLine.class,audioFormat);
+//			SourceDataLine line = (SourceDataLine)AudioSystem.getLine(info);
+//			AudioTrackJ2se audioTrackJ2se=new AudioTrackJ2se();
+//			audioTrackJ2se.setSourceDataLine(line);
+//			audioTrack=audioTrackJ2se;
+//		} catch (LineUnavailableException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		return audioTrack;
+	}
+}

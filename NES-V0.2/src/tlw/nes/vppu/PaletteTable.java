f98938e7878408919f4f8aedb37aafe333add8a7
tlw_ray <liwei.tang@magustek.com>
2012-11-12 11:07:58 +0800

    去掉java.awt.Color

--- NES-V0.2/src/tlw/nes/vppu/PaletteTable.java
+++ NES-V0.2/src/tlw/nes/vppu/PaletteTable.java
@@ -84,7 +84,7 @@ public class PaletteTable{
 	}
 	
 	public int HSLtoRGB(int h, int s, int l){
-		return Color.HSBtoRGB(h/255.0f,s/255.0f,l/255.0f);
+		return HSBtoRGB(h/255.0f,s/255.0f,l/255.0f);
 	}
 	
 	public int HSLtoRGB(int hsl){
@@ -93,10 +93,54 @@ public class PaletteTable{
 		h = (float)(((hsl>>16)&0xFF)/255d);
 		s = (float)(((hsl>> 8)&0xFF)/255d);
 		l = (float)(((hsl    )&0xFF)/255d);
-		return Color.HSBtoRGB(h,s,l);
+		return HSBtoRGB(h,s,l);
 		
 	}
-	
+    public static int HSBtoRGB(float hue, float saturation, float brightness) {
+		int r = 0, g = 0, b = 0;
+	    	if (saturation == 0) {
+		    r = g = b = (int) (brightness * 255.0f + 0.5f);
+		} else {
+		    float h = (hue - (float)Math.floor(hue)) * 6.0f;
+		    float f = h - (float)java.lang.Math.floor(h);
+		    float p = brightness * (1.0f - saturation);
+		    float q = brightness * (1.0f - saturation * f);
+		    float t = brightness * (1.0f - (saturation * (1.0f - f)));
+		    switch ((int) h) {
+		    case 0:
+			r = (int) (brightness * 255.0f + 0.5f);
+			g = (int) (t * 255.0f + 0.5f);
+			b = (int) (p * 255.0f + 0.5f);
+			break;
+		    case 1:
+			r = (int) (q * 255.0f + 0.5f);
+			g = (int) (brightness * 255.0f + 0.5f);
+			b = (int) (p * 255.0f + 0.5f);
+			break;
+		    case 2:
+			r = (int) (p * 255.0f + 0.5f);
+			g = (int) (brightness * 255.0f + 0.5f);
+			b = (int) (t * 255.0f + 0.5f);
+			break;
+		    case 3:
+			r = (int) (p * 255.0f + 0.5f);
+			g = (int) (q * 255.0f + 0.5f);
+			b = (int) (brightness * 255.0f + 0.5f);
+			break;
+		    case 4:
+			r = (int) (t * 255.0f + 0.5f);
+			g = (int) (p * 255.0f + 0.5f);
+			b = (int) (brightness * 255.0f + 0.5f);
+			break;
+		    case 5:
+			r = (int) (brightness * 255.0f + 0.5f);
+			g = (int) (p * 255.0f + 0.5f);
+			b = (int) (q * 255.0f + 0.5f);
+			break;
+		    }
+		}
+		return 0xff000000 | (r << 16) | (g << 8) | (b << 0);
+    }
 	public int getHue(int hsl){
 		return (hsl>>16)&0xFF;
 	}

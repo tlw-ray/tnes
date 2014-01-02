745ddb1fcac6c20e8968348d0846de3ef8bb7ad5
liwei <liwei@tlw_ray>
2014-01-02 22:04:18 +0800

    去除不用的方法

--- NES-V0.2/src/tlw/nes/vppu/Tile.java
+++ NES-V0.2/src/tlw/nes/vppu/Tile.java
@@ -18,12 +18,6 @@ public class Tile{
 		pix = new int[64];
 	}
 	
-//	public void setBuffer(short[] scanline){
-//		for(y=0;y<8;y++){
-//			setScanline(y,scanline[y],scanline[y+8]);
-//		}
-//	}
-	
 	public void setScanline(int sline, short b1, short b2){
 		initialized = true;
 		tIndex = sline<<3;
@@ -32,45 +26,6 @@ public class Tile{
 			if(pix[tIndex+x]==0)opaque[sline]=false;
 		}
 	}
-
-	public int[] renderSimple(int dx, int dy, int palAdd, int[] palette){
-		int[] result=new int[64];
-		tIndex=0;
-		fbIndex = (dy<<8)+dx;
-		for(y=8;y!=0;y--){
-			for(x=8;x!=0;x--){
-				palIndex = pix[tIndex];
-				if(palIndex != 0){
-					result[fbIndex] = palette[palIndex+palAdd];
-				}
-				fbIndex++;
-				tIndex++;
-			}
-			fbIndex-=8;
-			fbIndex+=256;
-		}
-		return result;
-	}
-	
-//	public void renderSmall(int dx,int dy,int[] buffer, int palAdd, int[] palette){
-//		tIndex = 0;
-//		fbIndex = (dy<<8)+dx;
-//		for(y=0;y<4;y++){
-//			for(x=0;x<4;x++){
-//				
-//				c = (palette[pix[tIndex]+palAdd]>>2)&0x003F3F3F;
-//				c += (palette[pix[tIndex+1]+palAdd]>>2)&0x003F3F3F;
-//				c += (palette[pix[tIndex+8]+palAdd]>>2)&0x003F3F3F;
-//				c += (palette[pix[tIndex+9]+palAdd]>>2)&0x003F3F3F;
-//				buffer[fbIndex] = c;
-//				fbIndex++;
-//				tIndex+=2;
-//			}
-//			tIndex+=8;
-//			fbIndex+=252;
-//		}
-//		
-//	}
 	
 	public void render(int srcx1, int srcy1, int srcx2, int srcy2, int dx, int dy, int[] fBuffer, int palAdd, int[] palette, boolean flipHorizontal, boolean flipVertical, int pri, int[] priTable){
 	
@@ -188,33 +143,6 @@ public class Tile{
 		}
 		
 	}
-	
-//	public boolean isTransparent(int x, int y){
-//		return (pix[(y<<3)+x]==0);
-//	}
-	
-//	public void dumpData(String file){
-//		
-//		try{
-//			
-//			File f = new File(file);
-//			FileWriter fWriter = new FileWriter(f);
-//		
-//			for(int y=0;y<8;y++){
-//				for(int x=0;x<8;x++){
-//					fWriter.write(Misc.hex8(pix[(y<<3)+x]).substring(1));
-//				}
-//				fWriter.write("\r\n");
-//			}
-//			
-//			fWriter.close();
-//			//System.out.println("Tile data dumped to file "+file);
-//			
-//		}catch(Exception e){
-//			//System.out.println("Unable to dump tile to file.");
-//			e.printStackTrace();
-//		}
-//	}
 	
 	public void stateSave(ByteBuffer buf){
 		

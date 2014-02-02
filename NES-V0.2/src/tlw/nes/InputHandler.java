2a0f6d26a45bbc6b4822ac38efe8ed571ff310c2
liwei <liwei@tlw_ray>
2014-02-02 22:26:30 +0800

    为InputHandler添加注释

--- NES-V0.2/src/tlw/nes/InputHandler.java
+++ NES-V0.2/src/tlw/nes/InputHandler.java
@@ -14,8 +14,23 @@ public interface InputHandler{
 	// Key count:
 	public static final int NUM_KEYS  =  8;
 	
+	/**
+	 * 获得当前输入键
+	 * @param padKey
+	 * @return
+	 */
 	public short getKeyState(int padKey);
+	
+	/**
+	 * 将输入设备的键与预设定的键进行映射
+	 * @param padKey 游戏手柄键
+	 * @param deviceKey 输入设备的键
+	 */
 	public void mapKey(int padKey, int deviceKey);
+	
+	/**
+	 * 键状态重置，输入设备进入未按下任何按钮的状态
+	 */
 	public void reset();
 	
 }
